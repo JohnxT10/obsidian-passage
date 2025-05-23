1b57c8e8548f118d80ac0336acd99925a9ce17fa
JohnxT10 <johnxthompson01@gmail.com>
2025-05-23 21:56:12 +0100

    Applied light and dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// Light/Dark Mode Toggle
+const btn = document.getElementById("theme-toggle");
+
+btn.addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+
+    // Change button text based on current mode
+    if (document.body.classList.contains("light-mode")) {
+        btn.textContent = "Dark Mode";
+    } else {
+        btn.textContent = "Light Mode";
+    }
+});
+
+
 function rand(max) {
     return Math.floor(Math.random() * max);
   }
